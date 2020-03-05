ba955d87ec545aa033c87628f7c591d38943c163
DougA <douganderson444@gmail.com>
2020-03-05 12:29:24 -0500

    editabletext

---
+++ src/components/editabletext.svelte
@@ -0,0 +1,58 @@
+<script>
+// https://codesandbox.io/s/quizzical-flower-prx95
+
+import { tick } from 'svelte';
+import {start} from './stores.js';
+// Props
+export let value = '';
+export let type = 'text';
+export let placeholder = 'Click to Edit';
+export let labelClasses = '';
+export let inputClasses = '';
+let editing = false;
+let inputEl;
+let label;
+// Computed
+$: isText = type === 'text';
+$: isNumber = type === 'number';
+$: if (isNumber) {
+      label = value === '' ? placeholder : value;
+    } else if (isText) {
+      label = value ? value : placeholder;
+    }
+const toggle = async (_) => {
+  editing = !editing;
+  if (editing) {
+    await tick();
+    inputEl.focus();
+  }
+};
+const handleInput = (e) => {
+  value = isNumber ? +e.target.value : e.target.value;
+};
+const handleEnter = (e) => {
+  if (e.keyCode === 13) inputEl.blur();
+};
+const handleBlur = (_) => {
+  toggle();
+};
+</script>
+{#if editing && (isText || isNumber)}
+  <input 
+    class={inputClasses}
+    bind:this={inputEl}
+    {type}
+    {value}
+    {placeholder}
+    on:input={handleInput}
+    on:keyup={handleEnter}
+    on:blur={handleBlur}
+    on:keydown={()=> {$start = new Date()}} 
+    >
+{:else}
+  <span 
+    class={labelClasses}
+    on:click={toggle}>
+  {label}
+  </span>
+{/if}
